80df8d409462a31945337a748bd5ca12df0d77ff
Jonathan Bowman <jonathanrbowman@me.com>
2018-07-31 21:03:01 -0400

    Added new modal library

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,22 @@
+var contact_app = contact_app || {};
+contact_app.modal = contact_app.modal || {}
+
+contact_app.modal.open = function(modal_id, callback = false) {
+  $(".c-modal[data-modal-id='" + modal_id + "']").addClass("is-active");
+  if (callback) {
+    callback();
+  }
+};
+
+contact_app.modal.close = function(callback = false) {
+  $(".c-modal").removeClass("is-active new-entry");
+  if (callback) {
+    callback();
+  }
+};
+
+$(function() {
+  $("body").on("click.openModal", ".js-modal-trigger", function() {
+    contact_app.modal.open($(this).data("modal-id"));
+  });
+});
---
+++ app/assets/javascripts/modal_compiled.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var contact_app = contact_app || {};
+contact_app.modal = contact_app.modal || {};
+
+contact_app.modal.open = function (modal_id) {
+  var callback = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+
+  $(".c-modal[data-modal-id='" + modal_id + "']").addClass("is-active");
+  if (callback) {
+    callback();
+  }
+};
+
+contact_app.modal.close = function () {
+  var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+
+  $(".c-modal").removeClass("is-active new-entry");
+  if (callback) {
+    callback();
+  }
+};
+
+$(function () {
+  $("body").on("click.openModal", ".js-modal-trigger", function () {
+    contact_app.modal.open($(this).data("modal-id"));
+  });
+});
